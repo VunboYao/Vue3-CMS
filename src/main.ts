92bd2e03be52fe8999c4e2ee38d99d6c4edfa992
Vunbo MacMini <vunbo@outlook.com>
2022-01-27 23:20:40 +0800

    feat: env config

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,7 @@ import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).mount('#app')
+
+// 环境变量
+console.error('VUE_APP_BASE_URL :>> ', process.env.VUE_APP_BASE_URL)
+console.error('VUE_APP_BASE_URL :>> ', process.env.VUE_APP_BASE_NAME)
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = {
+  // publicPath: './', // 打包后本地查看
   configureWebpack: {
     /*   resolve: {
       alias: {
