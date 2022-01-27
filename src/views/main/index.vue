82959c52dbc654c1a831fcf33adb046c762b9006
VunboYao <vunbo@outlook.com>
2022-01-27 17:40:10 +0800

    feat(element-plus): import on demand

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h2>Main</h2>
+    <el-button>Hello</el-button>
+    <el-button type="primary">Hello</el-button>
   </div>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,26 @@
-const path = require('path')
-
+// const path = require('path')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = {
-  /* configureWebpack: {
-    resolve: {
+  configureWebpack: {
+    /*   resolve: {
       alias: {
         views: '@/views'
       }
-    }
-  } */
-  chainWebpack: (config) => {
+    } */
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  }
+  /* chainWebpack: (config) => {
     config.resolve.alias
       .set('@', path.resolve(__dirname, 'src'))
       .set('views', '@/views')
-  }
+  } */
 }
