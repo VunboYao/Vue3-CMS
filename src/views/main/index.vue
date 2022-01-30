8e7834a5563ab8ae4a62375acf36d05761130a52
VunboYao <vunbo@outlook.com>
2022-01-31 01:26:26 +0800

    style(main): formating

---
+++ src/views/main/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Hello Main</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: '',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
