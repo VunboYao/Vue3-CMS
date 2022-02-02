ccea8766cb7bfebcaca9af9b2428a234236aec4b
VunboYao <vunbo@outlook.com>
2022-02-03 01:33:35 +0800

    fix(login/type): fix the type of function return value

--- src/service/login/login.ts
+++ src/service/login/login.ts
@@ -1,7 +1,7 @@
 import requestInstance from '@/service'
 import { iLoginRes, iLoginReq, iLoginResData } from '@/service/login/type'
 
-export function reqLogin(data: iLoginReq) {
+export function reqLogin(data: iLoginReq): Promise<iLoginRes> {
   // 返回一个由请求实例触发的post请求
   return requestInstance.post<iLoginRes<iLoginResData>>({
     url: '/login',
--- src/service/login/type.ts
+++ src/service/login/type.ts
@@ -4,7 +4,7 @@ export interface iLoginReq {
 }
 
 // 返回值类型的接口定义：可扩展,泛型实现，data未固定类型
-export interface iLoginRes<T> {
+export interface iLoginRes<T = any> {
   code: number
   data: T
 }
