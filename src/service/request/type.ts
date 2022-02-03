0aec65b10663ef184e77095d2eae038fd52c6abe
VunboYao <vunbo@outlook.com>
2022-02-03 23:52:54 +0800

    style(request/type.ts): code comments

--- src/service/request/type.ts
+++ src/service/request/type.ts
@@ -1,5 +1,6 @@
 import type { AxiosRequestConfig, AxiosResponse } from 'axios'
 
+// 拦截器定义
 export interface VunRequestInterceptors<T = AxiosResponse> {
   requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig
   requestInterceptorCatch?: (err: any) => any
@@ -7,7 +8,7 @@ export interface VunRequestInterceptors<T = AxiosResponse> {
   responseInterceptorCatch?: (err: any) => any
 }
 
-// 扩展AxiosRequestConfig.增加拦截器处理
+// 扩展AxiosRequestConfig.增加拦截器处理、loading等
 export interface VunRequestConfig<T = AxiosResponse>
   extends AxiosRequestConfig {
   interceptor?: VunRequestInterceptors<T>
