9fc8df836d798a7f2b1d6468e2e267480fa785d8
VunboYao <vunbo@outlook.com>
2022-02-07 17:30:28 +0800

    style: coder liene speartor

--- src/components/nav-menu/src/nav-menu.vue
+++ src/components/nav-menu/src/nav-menu.vue
@@ -1,1 +1,57 @@
-<template>  <div class="nav-menu">    <div class="logo">      <img src="~@/assets/img/logo.svg" alt="logo" class="img" />      <span class="title">Vue3+TS</span>    </div>    <el-menu      active-text-color="#ffd04b"      background-color="#286DAF"      class="el-menu-vertical-demo"      default-active="2"      text-color="#fff"    >    </el-menu>  </div></template><script lang="ts">import { defineComponent } from 'vue'export default defineComponent({  name: '',  setup() {    let lock = 'edit'    return {      lock    }  }})</script><style scoped lang="less">.nav-menu {  height: 100%;  background-color: #05294d;  .logo {    display: flex;    height: 28px;    padding: 12px 10px 8px 10px;    flex-direction: row;    justify-content: flex-start;    align-items: center;    .img {      height: 100%;      margin: 0 10px;    }    .title {      font-size: 16px;      font-weight: 700;      color: white;    }  }}</style>
+<template>
+  <div class="nav-menu">
+    <div class="logo">
+      <img src="~@/assets/img/logo.svg" alt="logo" class="img" />
+      <span class="title">Vue3+TS</span>
+    </div>
+    <el-menu
+      active-text-color="#ffd04b"
+      background-color="#286DAF"
+      class="el-menu-vertical-demo"
+      default-active="2"
+      text-color="#fff"
+    >
+    </el-menu>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: '',
+  setup() {
+    let lock = 'edit'
+    return {
+      lock
+    }
+  }
+})
+</script>
+
+<style scoped lang="less">
+.nav-menu {
+  height: 100%;
+  background-color: #05294d;
+
+  .logo {
+    display: flex;
+    height: 28px;
+    padding: 12px 10px 8px 10px;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+
+    .img {
+      height: 100%;
+      margin: 0 10px;
+    }
+
+    .title {
+      font-size: 16px;
+      font-weight: 700;
+      color: white;
+    }
+  }
+}
+</style>
