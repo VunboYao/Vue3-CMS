cd73d646d5d7ce47701f63a4716043645371c10a
Vunbo MacMini <vunbo@outlook.com>
2022-02-10 03:16:53 +0800

    feat: tableComponent

---
+++ src/base-ui/table/src/table.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <el-table :data="listData" border>
+      <template v-for="item in propsList" :key="item.prop">
+        <el-table-column v-bind="item" align="center">
+          <template #default="{ row }">
+            <slot :name="item.prop" :row="row">{{ row[item.prop] }}</slot>
+          </template>
+        </el-table-column>
+      </template>
+    </el-table>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Table',
+  props: {
+    listData: {
+      type: Array,
+      required: true
+    },
+    propsList: {
+      type: Array,
+      required: true
+    }
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="user">
     <page-search :form-config="formConfig" />
-    <div class="content"></div>
+    <div class="content">
+      <v-table :list-data="userList" :props-list="propsList">
+        <template #enable="{ row }">{{
+          row.enable ? '启用' : '禁用'
+        }}</template>
+      </v-table>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
 import { formConfig } from '@/views/main/system/user/config/search-config'
 import pageSearch from '@/components/page-search/page-search.vue'
 import { useStore } from '@/store'
+import vTable from '@/base-ui/table'
 
 export default defineComponent({
   name: 'user',
   components: {
-    pageSearch
+    pageSearch,
+    vTable
   },
   setup() {
     const store = useStore()
@@ -25,8 +33,19 @@ export default defineComponent({
         size: 1
       }
     })
+    const userList = computed(() => store.state.systemStore.userList)
+    const propsList = [
+      { prop: 'name', label: '用户名', minWidth: '100' },
+      { prop: 'realname', label: '真实姓名', minWidth: '120' },
+      { prop: 'cellphone', label: '手机号', minWidth: '100' },
+      { prop: 'enable', label: '状态', minWidth: '100' },
+      { prop: 'createAt', label: '创建时间', minWidth: '100' },
+      { prop: 'updateAt', label: '更新时间', minWidth: '100' }
+    ]
     return {
-      formConfig
+      formConfig,
+      propsList,
+      userList
     }
   }
 })
@@ -35,5 +54,8 @@ export default defineComponent({
 <style scoped>
 .user {
   padding: 18px 18px 0 0;
+}
+.content {
+  padding: 20px;
 }
 </style>
